4fd74ee31253a0ef3a74bd02c18838131a7ca5b1
Rishabh23112 <irishabh.k2311@gmail.com>
2026-06-20 20:43:35 +0530

    About

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <div className='min-h-screen  w-screen relative overflow-x-hidden'>
       <Hero/>
+      <About/>
     </div>
   )
 }
